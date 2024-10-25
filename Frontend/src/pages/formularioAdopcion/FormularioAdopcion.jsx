19e121ff879b3db49fbefa877ac4a329d0859a12
oscarhc3080 <oscarhc.3080@gmail.com>
2024-10-24 19:12:10 -0600

    Se modifica Formulario de Adopción y Estilos

--- Frontend/src/pages/formularioAdopcion/FormularioAdopcion.jsx
+++ Frontend/src/pages/formularioAdopcion/FormularioAdopcion.jsx
@@ -1,63 +1,135 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FormularioAdopcion.css';
 
-
 export const FormularioAdopcion = () => {
-    const homeImage= "/home_image.png"
-  return (
 
-    <div className="container--componente">
-    <div>
-        <img src={homeImage} alt="home_image" className="imagenFormulario" />
-    </div>
+    const homeImage = "/home_image.png"
 
-    {/* Formulario de Adopción */}
+    const [patio, setPatio] = useState('');
+    const [otrasMascotas, setOtrasMascotas] = useState('');
+    const [compromiso, setCompromiso] = useState('');
 
-    <div className="container--formulario" id="container--form">
-        <h2 className="title">Formulario de Adopción</h2>
+    const handlePatioChange = (event) => {
+        setPatio(event.target.value);
+    }
 
-        <div className="container--datos">
-            <label htmlFor="fechaSolicitud">Fecha de Solicitud</label>
-            <input type="date" id="fechaSolicitud" />
-            <br />
+    const handleMascotasChange = (event) => {
+        setOtrasMascotas(event.target.value);
+    }
 
-            <label htmlFor="fechaAprobacion">Fecha de Aprobación</label>
-            <input type="date" id="fechaAprobacion" />
-            <br />
+    const handleCompromisoChange = (event) => {
+        setCompromiso(event.target.value);
+    }
 
-            <label htmlFor="telefono">Número de Teléfono</label>
-            <input type="number" id="telefono" />
-            <br />
+    return (
 
-            <label htmlFor="tiempoSolo">Disposición de tiempo (Horas al día)</label>
-            <input type="number" id="tiempoSolo" />
-            <br />
+        <div className="container--componente">
+            <div>
+                <img src={homeImage} alt="home_image" className="imagenFormulario" />
+            </div>
 
-            <label htmlFor="tipoVivienda">Tipo de vivienda</label>
-            <input type="text" id="tipoVivienda" />
-            <br />
+            {/* Formulario de Adopción */}
 
-            <label htmlFor="patio">¿Cuénta con patio?</label>
-            <input type="checkbox" id="patio" />
-            <br />
+            <div className="container--formulario" id="container--form">
+                <h2 className="title">Formulario de Adopción</h2>
 
-            <label htmlFor="otrasMascotas">¿Tiene otras mascotas?</label>
-            <input type="checkbox" id="otrasMascotas" />
-            <br />
+                <div className="separator">
+                    <p>Completa la información solicitada</p>
+                </div>
 
-            <label htmlFor="compromiso">¿Acepta términos y condiciones?</label>
-            <input type="checkbox" id="compromiso" />
-            <br />
+                <div className="container--datos">
+                    <label htmlFor="fechaSolicitud">Fecha de Solicitud</label>
+                    <input type="date" id="fechaSolicitud" />
+                    <br />
 
+                    <label htmlFor="fechaAprobacion">Fecha de Aprobación</label>
+                    <input type="date" id="fechaAprobacion" />
+                    <br />
+
+                    <label htmlFor="telefono">Número de Teléfono</label>
+                    <input type="number" id="telefono" />
+                    <br />
+
+                    <label htmlFor="tiempoSolo">Disposición de tiempo (Horas al día)</label>
+                    <input type="number" id="tiempoSolo" />
+                    <br />
+
+                    <label htmlFor="tipoVivienda">Tipo de vivienda</label>
+                    <input type="text" id="tipoVivienda" />
+                    <br />
+
+                    <label htmlFor="patio">¿Cuentas con patio en casa?</label>
+                    <label>
+                        <input
+                            type="radio"
+                            value="Sí"
+                            checked={patio === 'Sí'}
+                            onChange={handlePatioChange}
+                        />
+                        Sí
+                    </label>
+                    <label>
+                        <input
+                            type="radio"
+                            value="No"
+                            checked={patio === 'No'}
+                            onChange={handlePatioChange}
+                        />
+                        No
+                    </label>
+                    <br />
+
+                    <label htmlFor="otrasMascotas">¿Tienes otras mascotas?</label>
+                    <label>
+                        <input
+                            type="radio"
+                            value="Sí"
+                            checked={otrasMascotas === 'Sí'}
+                            onChange={handleMascotasChange}
+                        />
+                        Sí
+                    </label>
+                    <label>
+                        <input
+                            type="radio"
+                            value="No"
+                            checked={otrasMascotas === 'No'}
+                            onChange={handleMascotasChange}
+                        />
+                        No
+                    </label>
+                    <br />
+
+                    <label htmlFor="otrasMascotas">¿Aceptas términos y condiciones?</label>
+                    <label>
+                        <input
+                            type="radio"
+                            value="Sí"
+                            checked={compromiso === 'Sí'}
+                            onChange={handleCompromisoChange}
+                        />
+                        Sí
+                    </label>
+                    <label>
+                        <input
+                            type="radio"
+                            value="No"
+                            checked={compromiso === 'No'}
+                            onChange={handleCompromisoChange}
+                        />
+                        No
+                    </label>
+                    <br />
+
+                </div>
+
+                <div id="boton-enviar">
+                    <button type="submit" id="enviar">Enviar</button>
+                </div>
+            </div>
         </div>
 
-        <div id="boton-enviar">
-            <button type="submit" id="enviar">Enviar</button>
-        </div>
-    </div>
-</div>
-
-  );
+    );
 };
 
 export default FormularioAdopcion;
