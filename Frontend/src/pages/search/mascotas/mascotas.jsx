e45949d97e2944da991b362e7e3079b07314d617
macauy <maca.rabosto@gmail.com>
2024-10-31 18:29:10 -0300

    filtros

--- Frontend/src/pages/search/mascotas/mascotas.jsx
+++ Frontend/src/pages/search/mascotas/mascotas.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import Search from "../search";
-import Filtros from "../../../Components/filtros/filtros";
+import Filtros, { FiltrosAplicados } from "../../../Components/filtros/filtros";
 import filtrosData from "../../../Components/filtros/filtrosConfig.js";
 import "./mascotas.css";
 import axios from "axios";
@@ -10,17 +10,9 @@ const Mascotas = () => {
   const location = useLocation();
   const [filters, setFilters] = useState(location.state || {});
   const [filtrosConfig, setFiltrosConfig] = useState(filtrosData);
-
-  const handleFilterClick = (titulo, atributo) => {
-    const value = atributo.value;
-    setFilters((prevFilters) => ({
-      ...prevFilters,
-      [titulo.toLowerCase()]: value,
-    }));
-  };
+  const [activeFilter, setActiveFilter] = useState(null);
 
   useEffect(() => {
-    console.log("antes de fetch ubicaciones");
     const fetchUbicaciones = async () => {
       try {
         const response = await axios.get("http://localhost:3000/api/ubicacion/localidadConMascotas");
@@ -48,14 +40,38 @@ const Mascotas = () => {
     fetchUbicaciones();
   }, []);
 
+  const handleFilterClick = (titulo, atributo) => {
+    const value = atributo.value;
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      [titulo.toLowerCase()]: value,
+    }));
+  };
+
+  const toggleFilter = (index) => {
+    setActiveFilter((prevActiveFilter) => (prevActiveFilter === index ? null : index));
+  };
+
   return (
     <div className="gato">
       <div className="filtros-container">
         {filtrosConfig.map((filtro, index) => (
           <div key={index}>
-            <Filtros filtro={filtro} onFilterChange={handleFilterClick} />
+            <Filtros filtro={filtro} onFilterChange={handleFilterClick} isActive={activeFilter === index} onToggle={() => toggleFilter(index)} />
           </div>
         ))}
+      </div>
+      <div className="filtros-aplicados-container">
+        <FiltrosAplicados
+          filters={filters}
+          onRemoveFilter={(key) =>
+            setFilters((prev) => {
+              const updatedFilters = { ...prev };
+              delete updatedFilters[key];
+              return updatedFilters;
+            })
+          }
+        />
       </div>
       <Search filters={filters} />
     </div>
