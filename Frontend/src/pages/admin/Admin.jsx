d5a74fab32467f62caa37b9bdf234cd4b9ad3f79
macauy <maca.rabosto@gmail.com>
2024-10-31 11:19:39 -0300

    eliminar usuarios

--- Frontend/src/pages/admin/Admin.jsx
+++ Frontend/src/pages/admin/Admin.jsx
@@ -2,30 +2,30 @@ import { useContext } from "react";
 import TablaMascotas from "../../Components/TablaMascotas/tablaMascotas";
 import { MascotasContext } from "../../context/Mascotascontext";
 import TablaUser from "../../Components/tablaUsuarios/TablaUsuarios";
-import { UserContext } from '../../context/UserContext';
-import './Admin.css'
+import { UserContext } from "../../context/UserContext";
+import "./Admin.css";
 
 function Admin() {
-    const { mascotas } = useContext(MascotasContext);
-    const { users, user } = useContext(UserContext);
+  const { mascotas } = useContext(MascotasContext);
+  const { users } = useContext(UserContext);
 
-    console.log("usuarios en admin", users);
+  console.log("usuarios en admin", users);
 
-    return (
-        <>
-            <div className="admin-container">
-                <div className="tabla-mascotas">
-                    <h2>Lista de mascotas</h2>
-                    <TablaMascotas mascotas={mascotas} />
-                </div>
+  return (
+    <>
+      <div className="admin-container">
+        <div className="tabla-mascotas">
+          <h2>Lista de mascotas</h2>
+          <TablaMascotas mascotas={mascotas} />
+        </div>
 
-                <div className="tabla-usuarios">
-                    <h1>Lista de usuarios</h1>
-                    <TablaUser users={users} />
-                </div>
-            </div>
-        </>
-    );
+        <div className="tabla-mascotas">
+          <h1>Lista de usuarios</h1>
+          <TablaUser users={users} />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Admin;
